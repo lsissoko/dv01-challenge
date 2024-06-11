7e0df886aad0c1feecc7a12644da25c06a4540b7
Lamine Sissoko <1082131+lsissoko@users.noreply.github.com>
2024-06-11 12:56:11 -0400

    remove the GET endpoint

--- app/controllers/LoanController.scala
+++ app/controllers/LoanController.scala
@@ -13,12 +13,6 @@ class LoanController @Inject()(val controllerComponents: ControllerComponents) e
 
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
-  def find(limit: Int) = Action.async { implicit req =>
-    LoanStatsDAO.find(limit).map { result =>
-      Ok(Json.toJson(result))
-    }
-  }
-
   /**
     * Returns loan stats from the database matching the given [[LoanStatsSearch]].
     *
--- test/controllers/LoanControllerSpec.scala
+++ test/controllers/LoanControllerSpec.scala
@@ -15,63 +15,6 @@ class LoanControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
   implicit val actorSystem: ActorSystem = ActorSystem()
 
-  "LoanController GET" should {
-
-    "return an empty list when the limit is negative" in {
-      val controller = new LoanController(stubControllerComponents())
-      val actual = controller.find(-1234).apply(FakeRequest(GET, "/api/loans"))
-
-      val expected = Seq.empty[LoanStat]
-
-      status(actual) mustBe OK
-      contentType(actual) mustBe Some("application/json")
-      contentAsJson(actual) mustEqual Json.toJson(expected)
-    }
-
-    "return an empty list when the limit is zero" in {
-      val controller = new LoanController(stubControllerComponents())
-      val actual = controller.find(0).apply(FakeRequest(GET, "/api/loans"))
-
-      val expected = Seq.empty[LoanStat]
-
-      status(actual) mustBe OK
-      contentType(actual) mustBe Some("application/json")
-      contentAsJson(actual) mustEqual Json.toJson(expected)
-    }
-
-    "return the first N loans from the database" in {
-      val controller = new LoanController(stubControllerComponents())
-      val actual = controller.find(2).apply(FakeRequest(GET, "/api/loans"))
-
-      val expected = Seq(
-        LoanStat(
-          id=126285300,
-          loanAmount=Some(40000),
-          date=Some("Dec-2017"),
-          state=Some("CA"),
-          grade=Some("A"),
-          subGrade=Some("A2"),
-          ficoRangeLow=Some(780),
-          ficoRangeHigh=Some(784),
-        ),
-        LoanStat(
-          id=126367373,
-          loanAmount=Some(24000),
-          date=Some("Dec-2017"),
-          state=Some("IN"),
-          grade=Some("C"),
-          subGrade=Some("C4"),
-          ficoRangeLow=Some(705),
-          ficoRangeHigh=Some(709),
-        ),
-      )
-
-      status(actual) mustBe OK
-      contentType(actual) mustBe Some("application/json")
-      contentAsJson(actual) mustEqual Json.toJson(expected)
-    }
-  }
-
   "LoanController POST" should {
     "return an empty list when the limit is negative" in {
       val controller = new LoanController(stubControllerComponents())
